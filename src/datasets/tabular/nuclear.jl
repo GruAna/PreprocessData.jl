23ccd0b0317d41490c8d9df52d73939baefc1bca
GruAna <gru.ana@centrum.cz>
2021-06-19 16:46:46 +0200

    nuclear dataset message

--- src/datasets/tabular/nuclear.jl
+++ src/datasets/tabular/nuclear.jl
@@ -2,7 +2,8 @@ struct Nuclear <: Tabular end
 url(::Nuclear) = "https://github.com/JuliaStats/RDatasets.jl/raw/master/data/boot/nuclear.csv.gz"
 checksum(::Nuclear) = "0283f0f95a2df8e66ea40035483b6dcecb11fd856823f190ea943d774d0b9a44"
 prep(::Nuclear) = path -> preprocess(extract(path), Nuclear())
-target(::Nuclear) = 11
+target(::Nuclear) = 1
 headers(::Nuclear) = 1
 size(::Nuclear) = (32, 0, 0)
-problem(::Nuclear) = Classification
+problem(::Nuclear) = Regression
+message(::Nuclear) = "Nuclear Power Station Construction Data"
