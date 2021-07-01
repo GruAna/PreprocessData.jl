1e69aa43c31eff4b67e03b468ec2991152df1f1e
GruAna <gru.ana@centrum.cz>
2021-07-01 22:24:58 +0200

    dataset names

--- src/datasets/tabular/carevaluation.jl
+++ src/datasets/tabular/carevaluation.jl
@@ -1,8 +1,8 @@
-struct Carevaluation <: Tabular end
-url(::Carevaluation) = "http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
-checksum(::Carevaluation) = "b703a9ac69f11e64ce8c223c0a40de4d2e9d769f7fb20be5f8f2e8a619893d83"
-prep(::Carevaluation) = path -> preprocess(path, Carevaluation())
-target(::Carevaluation) = 7
-categorical(::Carevaluation) = 1:6
-size(::Carevaluation) = (1728, 0, 0)
-problem(::Carevaluation) = Classification
+struct CarEvaluation <: Tabular end
+url(::CarEvaluation) = "http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data"
+checksum(::CarEvaluation) = "b703a9ac69f11e64ce8c223c0a40de4d2e9d769f7fb20be5f8f2e8a619893d83"
+prep(::CarEvaluation) = path -> preprocess(path, CarEvaluation())
+target(::CarEvaluation) = 7
+categorical(::CarEvaluation) = 1:6
+size(::CarEvaluation) = (1728, 0, 0)
+problem(::CarEvaluation) = Classification
--- src/datasets/tabular/forestfires.jl
+++ src/datasets/tabular/forestfires.jl
@@ -1,9 +1,9 @@
-struct Forestfires <: Tabular end
-url(::Forestfires) = "https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv"
-checksum(::Forestfires) = "0d6586a1fa52f55bef48578aef14eb97273f1e9330e1a53423df497a77065253"
-prep(::Forestfires) = path -> preprocess(path, Forestfires())
-target(::Forestfires) = 13
-headers(::Forestfires) = 1
-categorical(::Forestfires) = [3;4]
-size(::Forestfires) = (517, 0, 0)
-problem(::Forestfires) = Regression
+struct ForestFires <: Tabular end
+url(::ForestFires) = "https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv"
+checksum(::ForestFires) = "0d6586a1fa52f55bef48578aef14eb97273f1e9330e1a53423df497a77065253"
+prep(::ForestFires) = path -> preprocess(path, ForestFires())
+target(::ForestFires) = 13
+headers(::ForestFires) = 1
+categorical(::ForestFires) = [3;4]
+size(::ForestFires) = (517, 0, 0)
+problem(::ForestFires) = Regression
